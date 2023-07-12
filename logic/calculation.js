f0ee1125db8412913792dfa882d2895273132d59
David Ford <dford215@gmail.com>
2023-07-12 10:56:05 -0400

    add calculation function

---
+++ logic/calculation.js
@@ -0,0 +1,14 @@
+function calculateGolfHandicap(scores, courseSlope, slopeRating) {
+    // Calculate the average score for the rounds
+    const averageScore = scores.reduce((total, score) => total + score) / scores.length;
+    
+    // Calculate the differential based on the average score and course slope
+    const differential = (averageScore - courseSlope) * 113 /  slopeRating;
+    
+    // Calculate the handicap based on the differential
+    const handicap = Math.round(differential * 0.96); // Adjusted for 96% handicap factor
+    
+    return handicap;
+  }
+
+  module.exports = { calculateGolfHandicap };
