c93d3fbfb6112c2e3898f1a7678cd397c3a35926
David Ford <dford215@gmail.com>
2023-07-14 21:44:06 -0400

    split list of scores and call handicap calculation

--- logic/calculation.js
+++ logic/calculation.js
@@ -1,9 +1,9 @@
-function calculateGolfHandicap(scores, courseSlope, slopeRating) {
+function calculateGolfHandicap(scores, courseRating, slopeRating) {
     // Calculate the average score for the rounds
     const averageScore = scores.reduce((total, score) => total + score) / scores.length;
     
     // Calculate the differential based on the average score and course slope
-    const differential = (averageScore - courseSlope) * 113 /  slopeRating;
+    const differential = (averageScore - courseRating) * 113 /  slopeRating;
     
     // Calculate the handicap based on the differential
     const handicap = Math.round(differential * 0.96); // Adjusted for 96% handicap factor
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,26 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import { useState } from 'react';
+import { calculateGolfHandicap } from '../logic/calculation';
 
 export default function Home() {
+  const [slopeRating, setSlopeRating] = useState(0.0);
+  const [courseRating, setCourseRating] = useState(0.0);
+  const [scoresInput, setScoresInput] = useState("");
+  const [handicap, setHandicap] = useState(0.0);
+
+  function performCalculation(){
+    console.log(scoresInput);
+    const scores = scoresInput.split(",");
+    const result = calculateGolfHandicap(scores, courseRating, slopeRating);
+    setHandicap(result);
+
+    setSlopeRating(0.0);
+    setCourseRating(0.0);
+    setScoresInput("");
+  }
+
   return (
     <div className="container">
       <Head>
@@ -13,8 +31,22 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Enter a comma seperated list of 9 hole golf scores.
         </p>
+        <input id="scoreListInput" value={scoresInput} onChange={e => {setScoresInput(e.currentTarget.value)}}></input>
+
+        <p className="description">
+          Enter the course slope rating
+        </p>
+        <input id="courseSlopeRatingInput" value={slopeRating} onChange={e => {setSlopeRating(e.currentTarget.value)}}></input>
+
+        <p className="description">
+          Enter the course rating
+        </p>
+        <input id="courseRatingInput" value={courseRating} onChange={ e => {setCourseRating(e.currentTarget.value)}}></input>
+        <br/>
+        <button id="calculateHandicapBtn" onClick={() => performCalculation()}>Submit</button>
+        <p className="description">Handicap: {handicap}</p>
       </main>
 
       <Footer />
